6b9ef5489257530b9d02ce099542fd2849834d1e
Kamlesh Mandal <kamleshmandal2322@gmail.com>
2022-01-12 11:24:43 +0530

    First Demo

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+    //public static final String INVALID_VALID_VALUE = "INVALID VALUE";
+    public static void main(String[] args) {
+       area(-1);
+       area(3,2);
+    }
+
+        public static double area(double radius) {
+
+            if (radius < 0) {
+                return -1.0;
+            }
+
+            return radius * radius * Math.PI;
+        }
+
+        public static double area(double x, double y) {
+
+            if ((x < 0) || (y < 0) || (x < 0 && y < 0)) {
+
+                return -1.0;
+            }
+
+            return x * y;
+        }
+    }
